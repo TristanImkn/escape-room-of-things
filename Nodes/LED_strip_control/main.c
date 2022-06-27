84504f10878ba535496d3123ff9d77714bb0abcb
TristanImkn <tristan.imken@haw-hamburg.de>
2022-06-27 14:48:29 +0200

    code for running LED puzzle, coap module has to be added

--- Nodes/LED_strip_control/main.c
+++ Nodes/LED_strip_control/main.c
@@ -24,12 +24,13 @@
 
 #include "xtimer.h"
 #include "color.h"
-
+#include "event.h"
+#include "event/periodic.h"
 #include "apa102.h"
 #include "apa102_params.h"
 #include "periph/gpio.h"
 
-
+int pos = 0;
 
 /**
  * @brief   Switch to the next LED every 10ms
@@ -51,12 +52,13 @@
  */
 static apa102_t dev;
 
+gpio_t button = GPIO_PIN(0, 12);
 /**
  * @brief   Allocate a color_rgb_t struct for each LED on the strip
  */
 static color_rgba_t leds[APA102_PARAM_LED_NUMOF];
-
-gpio_t button = GPIO_PIN(0, 11);
+static event_periodic_t event_periodic;
+static event_queue_t event_queue;
 
 static void setcolor(int color, uint8_t alpha)
 {
@@ -77,109 +79,102 @@ static void setcolor(int color, uint8_t alpha)
     }
 }
 
-
-
-void button_callback (void *arg)
+static void button_handler(event_t *event)
 {
-    (void) arg; /* the argument is not used */
-
-    if (!gpio_read(button)) {
-
+    (void)event;
+    puts("Button!");
+    printf("pos: %d\n", pos);
+    if (pos == 4) {
+        puts("You win");
+        event_periodic_stop(&event_periodic);
+        event_queue_init(&event_queue);
+        for (unsigned i = 0; i <= UINT8_MAX; i += BSTEP) {
+            setcolor(1, (uint8_t)i);
+            apa102_load_rgba(&dev, leds);
+            xtimer_usleep(DIM);
+        }
     }
     else {
-
+        memset(leds, 0, sizeof(color_rgba_t) * APA102_PARAM_LED_NUMOF);
+        for (unsigned i = 0; i < APA102_PARAM_LED_NUMOF; i++) {
+           leds[i].alpha = 255;
+        }
+        pos = 0;
+        /* apply the values to the LED strip */
+        apa102_load_rgba(&dev, leds);
     }
+}
+
+static void timeout_handler(event_t *event)
+{
+    (void)event;
+    static int step = 1;
+    static color_hsv_t col = { 0.0, 1.0, 1.0 };
+    printf("Timeout!\n");
+    printf("pos: %d\n", pos);
+
+    /* change the active color by running around the hue circle */
+    col.h += 1.0;
+    if (col.h > 360.0) {
+        col.h = 0.0;
+    }
+    /* set the active LED to the active color value */
+    memset(&leds[pos].color, 0, sizeof(color_rgb_t));
+
+    /* switch direction once reaching an end of the strip */
+    if (!pos) {
+        step = 1;
+    }
+    else if (pos == (APA102_PARAM_LED_NUMOF - 1)) {
+        step = -1;
+    }
+    pos += step;
+
+    color_hsv2rgb(&col, &leds[pos].color);
+
+    /* apply the values to the LED strip */
+    apa102_load_rgba(&dev, leds);
+
+    puts("here");   
+}
+
+static event_t button_event = { .handler = button_handler };
+static event_t timeout_event = { .handler = timeout_handler };
+
+void button_callback(void *arg)
+{
+    (void) arg; /* the argument is not used */
+    event_post(&event_queue, &button_event);
 }
 
 int main(void)
 {
-    int pos = 0;
-    int step = 1;
-    color_hsv_t col = { 0.0, 1.0, 1.0 };
+    gpio_mode_t input_mode = GPIO_IN_PU;
+    gpio_init(button, input_mode);
 
     puts("APA102 Test App");
 
     /* initialize all LED color values to black (off) */
     memset(leds, 0, sizeof(color_rgba_t) * APA102_PARAM_LED_NUMOF);
+    for (unsigned i = 0; i < APA102_PARAM_LED_NUMOF; i++) {
+        leds[i].alpha = 255;
+    }
 
     /* initialize the driver */
     apa102_init(&dev, &apa102_params[0]);
 
-    gpio_init_int(button, GPIO_IN_PU, GPIO_BOTH, button_callback, NULL);
+    gpio_init_int(button, GPIO_IN_PU, GPIO_FALLING, button_callback, NULL);
 
     /* reset color values */
-    setcolor(-1, 255);
     apa102_load_rgba(&dev, leds);
+
+    event_queue_init(&event_queue);
 
     puts("Initialization done.");
 
-    /* set to each red, green, and blue, and fade each color in and out */
-    /*for (int col = 0; col <= 2; col++) {
-        int i = 0;
-        for (; i <= (int)UINT8_MAX; i += BSTEP) {
-            setcolor(col, (uint8_t)i);
-            apa102_load_rgba(&dev, leds);
-            xtimer_usleep(DIM);
-        }
-        i -= BSTEP;
-        for (; i >= 0; i -= BSTEP) {
-            setcolor(col, (uint8_t)i);
-            apa102_load_rgba(&dev, leds);
-            xtimer_usleep(DIM);
-        }
-    }
-
-    puts("Color Fading done");*/
-
-    xtimer_ticks32_t now = xtimer_now();
-
-    while(1){
-        if (!gpio_read(button)){
-            puts("break");
-
-            if (pos == 4) {
-                
-                    int i = 0;
-                    for (; i <= (int)UINT8_MAX; i += BSTEP) {
-                        setcolor(1, (uint8_t)i);
-                        apa102_load_rgba(&dev, leds);
-                        xtimer_usleep(DIM);
-                        }
-                
-                break;
-            }
-            else {
-                xtimer_msleep(1000);
-                pos = 0;
-                continue;
-            }
-            
-        }
-        else {
-            
-        }
-
-        /* change the active color by running around the hue circle */
-        col.h += 1.0;
-        if (col.h > 360.0) {
-            col.h = 0.0;
-        }
-
-        /* set the active LED to the active color value */
-        memset(&leds[pos].color, 0, sizeof(color_rgb_t));
-        pos += step;
-        color_hsv2rgb(&col, &leds[pos].color);
-
-        /* apply the values to the LED strip */
-        apa102_load_rgba(&dev, leds);
-
-        /* switch direction once reaching an end of the strip */
-        if ((pos == (APA102_PARAM_LED_NUMOF - 1)) || (pos == 0)) {
-            step *= -1;
-        }
-
-        xtimer_periodic_wakeup(&now, STEP);
-    }
+    event_periodic_init(&event_periodic, ZTIMER_USEC, &event_queue, (event_t*)&timeout_event);
+    event_periodic_start(&event_periodic, STEP);
+    event_loop(&event_queue);
 
     return 0;
 }
